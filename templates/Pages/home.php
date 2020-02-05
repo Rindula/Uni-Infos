a2d3b42eaa1005bd0aef37366572d4ac6e7b5d3b
Sven Nolting <service@rindula.de>
2020-02-05 10:09:59 +0100

    Modified links

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -7,5 +7,5 @@
 <h2 style="width: 100%; text-align: center">Links</h2>
 <div style="text-align: center">
 <?= $this->Html->link("Zum Stundenplan", ['controller' => 'stundenplan', 'action' => 'index'], ['class' => 'button button-clear']) ?>
-<?= $this->Html->link("Zum Dateispeicher", 'https://files.rindula.de/nextcloud/index.php/s/Fj3YTt9Mcga287L', ['class' => 'button button-outline']) ?>
+<?= $this->Html->link("Zum Dateispeicher", 'https://files.rindula.de/nextcloud/index.php/s/Fj3YTt9Mcga287L', ['class' => 'button button-outline', 'target' => '_blank']) ?>
 </div>
