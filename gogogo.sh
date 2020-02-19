beba129204986ad76519a957166e9df2c562a7bf
Sven Nolting <service@rindula.de>
2020-02-19 10:49:37 +0100

    Added maintenancemode on Update

--- gogogo.sh
+++ gogogo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /var/www/vhosts/rindula.de/uni.rindula.de/
 
+/usr/sbin/plesk bin domain --suspend uni.rindula.de
+
 # Composer Update
 /opt/plesk/php/7.2/bin/php /usr/lib/plesk-9.0/composer.phar update
 
@@ -10,3 +12,5 @@ cd /var/www/vhosts/rindula.de/uni.rindula.de/
 
 ## Cache leeren
 /opt/plesk/php/7.2/bin/php ./bin/cake.php cache clear_all
+
+/usr/sbin/plesk bin domain --on uni.rindula.de
