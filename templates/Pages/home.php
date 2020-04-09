b68542943762e6ae4fc6f33911ffee3845fe7482
Sven Nolting <service@rindula.de>
2020-04-09 09:48:51 +0200

    Sitzungsaufzeichnungen verlinkt

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -9,6 +9,7 @@ use App\View\AppView; ?>
 <div style="text-align: center">
     <?= $this->Html->link("Zum Stundenplan", ['controller' => 'stundenplan', 'action' => 'index'], ['class' => 'button button-clear']) ?>
     <?= $this->Html->link("Zum Dateispeicher", 'https://files.rindula.de/nextcloud/index.php/s/GEEgyjNm6zS7xE9', ['class' => 'button button-outline', 'target' => '_blank']) ?>
+    <?= $this->Html->link("Sitzungsaufzeichnugnen", 'https://files.rindula.de/nextcloud/index.php/s/z4mi9YMsr74s3AQ', ['class' => 'button button-outline', 'target' => '_blank']) ?>
     <br>
     <?= $this->Html->link("<img style='width: 200px' alt='Jetzt bei Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/de_badge_web_generic.png'/>", 'https://play.google.com/store/apps/details?id=de.rindula.uniinfos&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1', ['target' => '_blank', 'escape' => false]) ?>
 </div>
