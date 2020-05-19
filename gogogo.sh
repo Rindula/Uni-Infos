0b01fbcf4c7ec215cd83094885d9249cf8944881
Sven Nolting <service@rindula.de>
2020-05-19 16:31:08 +0200

    Anpassungen des Deployscripts

--- gogogo.sh
+++ gogogo.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-cd /var/www/vhosts/rindula.de/uni.rindula.de/
+cd /var/www/vhosts/rindula.de/uni.rindula.de/ || exit
 
 /usr/sbin/plesk bin domain --suspend uni.rindula.de
+/usr/sbin/plesk bin domain --suspend dh.rindula.de
+/usr/sbin/plesk bin domain --suspend dhbw.rindula.de
 
 # Composer Update
-/opt/plesk/php/7.2/bin/php /usr/lib/plesk-9.0/composer.phar update
+/opt/plesk/php/7.2/bin/php /usr/lib/plesk-9.0/composer.phar install --dev --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-scripts --no-suggest
 
 # Cake
 ## Datenbank Migration
@@ -14,3 +16,5 @@ cd /var/www/vhosts/rindula.de/uni.rindula.de/
 /opt/plesk/php/7.2/bin/php ./bin/cake.php cache clear_all
 
 /usr/sbin/plesk bin domain --on uni.rindula.de
+/usr/sbin/plesk bin domain --on dh.rindula.de
+/usr/sbin/plesk bin domain --on dhbw.rindula.de
