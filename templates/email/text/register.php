9b1d1a8ebf6a9e6d3fb5e9e0d7334721d09d0b05
Sven Nolting <service@rindula.de>
2020-06-02 13:24:54 +0200

    Registrierungsmail Anpassung

--- templates/email/text/register.php
+++ templates/email/text/register.php
@@ -4,5 +4,6 @@ um deine Registrierung abzuschließen folge bitte diesem Link:
 
 <?= $link ?>
 
+Dadurch wirst du nach Administrativer freischaltung in den Verteiler eingetragen!
 
 Mit freundlichen Grüßen
