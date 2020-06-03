3cb2dd9d83580d3ab6a9ba856d601a50b6e00324
Sven Nolting <service@rindula.de>
2020-06-03 14:13:57 +0200

    Anpassung deployscript

--- gogogo.sh
+++ gogogo.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 cd /var/www/vhosts/rindula.de/uni.rindula.de/ || exit
 
-/usr/sbin/plesk bin domain --suspend uni.rindula.de
-/usr/sbin/plesk bin domain --suspend dh.rindula.de
-/usr/sbin/plesk bin domain --suspend dhbw.rindula.de
-
-# Warte kurz ... Das suspendieren dauert kurz
-sleep 10s
-
 # Composer Update
 /opt/plesk/php/7.2/bin/php /usr/lib/plesk-9.0/composer.phar install --dev --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-scripts --no-suggest
 
@@ -17,7 +10,3 @@ sleep 10s
 
 ## Cache leeren
 /opt/plesk/php/7.2/bin/php ./bin/cake.php cache clear_all
-
-/usr/sbin/plesk bin domain --on uni.rindula.de
-/usr/sbin/plesk bin domain --on dh.rindula.de
-/usr/sbin/plesk bin domain --on dhbw.rindula.de
