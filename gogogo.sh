eb565b6ad9d87315a83abcc1d7b7ff5e6bb408aa
Sven Nolting <service@rindula.de>
2020-09-20 09:47:57 +0000

    Revert "Update gogogo.sh"
    This reverts commit a7c9e94a7853026ac68a4f6338bfbe443a4d2f96

--- gogogo.sh
+++ gogogo.sh
@@ -3,14 +3,12 @@ cd /var/www/vhosts/rindula.de/uni.rindula.de/ || exit
 
 chmod +x ./bin/cake
 
-export php=/opt/plesk/php/7.4/bin/php
-
 # Composer Update
-/usr/bin/composer install --dev --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-scripts --no-suggest
+/opt/plesk/php/7.4/bin/php /usr/lib/plesk-9.0/composer.phar install --dev --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-scripts --no-suggest
 
 # Cake
 ## Datenbank Migration
-php ./bin/cake.php migrations migrate
+/opt/plesk/php/7.4/bin/php ./bin/cake.php migrations migrate
 
 ## Cache leeren
-php ./bin/cake.php cache clear_all
+/opt/plesk/php/7.4/bin/php ./bin/cake.php cache clear_all
