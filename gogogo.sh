dcc2509fb939eb94cc98f230589c0ee855d48027
Sven Nolting <service@rindula.de>
2020-09-20 09:40:55 +0000

    Update gogogo.sh

--- gogogo.sh
+++ gogogo.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 cd /var/www/vhosts/rindula.de/uni.rindula.de/ || exit
 
+chmod +x ./bin/cake
+
 # Composer Update
-/opt/plesk/php/7.2/bin/php /usr/lib/plesk-9.0/composer.phar install --dev --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-scripts --no-suggest
+/opt/plesk/php/7.4/bin/php /usr/lib/plesk-9.0/composer.phar install --dev --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-scripts --no-suggest
 
 # Cake
 ## Datenbank Migration
-/opt/plesk/php/7.2/bin/php ./bin/cake.php migrations migrate
+/opt/plesk/php/7.4/bin/php ./bin/cake.php migrations migrate
 
 ## Cache leeren
-/opt/plesk/php/7.2/bin/php ./bin/cake.php cache clear_all
+/opt/plesk/php/7.4/bin/php ./bin/cake.php cache clear_all
