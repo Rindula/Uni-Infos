43a41c59c39ec5a8d390c4a257586de744088f49
Sven Nolting <service@rindula.de>
2020-09-20 12:27:27 +0000

    Update Deployscript

--- gogogo.sh
+++ gogogo.sh
@@ -4,7 +4,7 @@ cd /var/www/vhosts/rindula.de/uni.rindula.de/ || exit
 chmod +x ./bin/cake
 
 # Composer Update
-/opt/plesk/php/7.4/bin/php /usr/lib/plesk-9.0/composer.phar install --dev --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-scripts --no-suggest
+/usr/bin/composer install --dev --no-ansi --optimize-autoloader --no-interaction --no-plugins --no-progress --no-scripts --no-suggest
 
 # Cake
 ## Datenbank Migration
