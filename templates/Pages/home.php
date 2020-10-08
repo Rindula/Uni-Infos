3f51127c0735cc01e244052ffd2c50ce6e8ec82f
Sven Nolting <service@rindula.de>
2020-10-08 19:54:38 +0000

    Linkanpassungen

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -7,9 +7,8 @@ use App\View\AppView; ?>
 <h1 style="width: 100%; text-align: center">UniInfos</h1>
 <h2 style="width: 100%; text-align: center">Links</h2>
 <div style="text-align: center">
-    <?= $this->Html->link(__("To file storage"), 'https://files.rindula.de/nextcloud/index.php/s/TqWQwraiBN3JJoZ', ['class' => 'button button-outline', 'target' => '_blank']) ?>
+    <?= $this->Html->link(__("To file storage"), 'https://files.rindula.de/nextcloud/index.php/s/FffjYnKGNjYiNMb', ['class' => 'button button-outline', 'target' => '_blank']) ?>
     <?= $this->Html->link(__("To the timetable"), ['controller' => 'stundenplan', 'action' => 'index'], ['class' => 'button button-clear']) ?>
-    <?= $this->Html->link(__("Session recordings"), 'https://files.rindula.de/nextcloud/index.php/s/z4mi9YMsr74s3AQ', ['class' => 'button button-outline', 'target' => '_blank']) ?>
     <!--    <br>-->
     <?= ""//$this->Html->link("<img style='width: 200px' alt='Jetzt bei Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/de_badge_web_generic.png'/>", 'https://play.google.com/store/apps/details?id=de.rindula.uniinfos&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1', ['target' => '_blank', 'escape' => false])  ?>
 </div>
