abdd36c9f2c517c3e951a2b5da1b0d320d7a3de3
Sven Nolting <rindula@users.noreply.github.com>
2021-03-15 08:14:28 +0100

    refactor(Stundenplan): Change JavaScript Html to template copy
    Previously the HTML on the Page was generated by JavaScript, which was a
    bit hard to read or modify. Now there is a Template Tag within the index
    which gets cloned for each enty and its data gets filled out. This is
    way better to maintain than before.

--- templates/Stundenplan/index.php
+++ templates/Stundenplan/index.php
@@ -18,3 +18,36 @@ use App\View\AppView; ?>
     <div id="list">
     </div>
 </div>
+<template id="lessonTemplate">
+    <blockquote>
+        <div class="row row-top">
+            <span data-role="title" class="column column-20">TITLE</span>
+            <span data-role="location" class="column-offset-50 column-30 column"
+                  style='text-align: right'>LOCATION</span>
+        </div>
+        <div class='row'>
+            <small data-role="description" class='column'>DESCRIPTION</small>
+        </div>
+        <br>
+        <div data-role="notes" class='message'>NOTES</div>
+        <div data-role="loggedinnotes" class='message'>LOGGEDINNOTES</div>
+        <a data-role="editnotes" class='' href='#'>📝</a><br>
+        <div data-role="deletenotes">
+            <?= $this->Html->link('Notiz löschen', ['controller' => 'stundenplan', 'action' => 'delete', '__UID__', 'note'], ['confirm' => 'Bist du sicher, dass du die Notiz löschen willst?']) . "<br>" . $this->Html->link('Eingeloggten Notiz löschen', ['controller' => 'stundenplan', 'action' => 'delete', '__UID__', 'loggedInNote'], ['confirm' => 'Bist du sicher, dass du die Eingeloggten Notiz löschen willst?']) . "<br>" . $this->Html->link('Alle Notizen löschen', ['controller' => 'stundenplan', 'action' => 'delete', '__UID__', 'all'], ['confirm' => 'Bist du sicher, dass du die alle Notizen dieser Stunde löschen willst?']) ?>
+        </div>
+        <div class='row mobile-margin-down'>
+            <div class='column column-20'>Beginn:</div>
+            <div data-role="beginntime" class='column column-80'>BEGINNTIME</div>
+        </div>
+        <div class='row'>
+            <div class='column column-20'>Ende:</div>
+            <div data-role="endtime" class='column column-80'>ENDTIME</div>
+        </div>
+        <br>
+        <div data-role="progress-wrapper" class='column'>
+            <div class="progress">
+                <div data-role="progressbar" class="progress-value" data-percent='' style="width: 0">0%</div>
+            </div>
+        </div>
+    </blockquote>
+</template>
--- webroot/js/pages/stundenplan.js
+++ webroot/js/pages/stundenplan.js
@@ -14,74 +14,125 @@ function loadData() {
 }
 
 function setData(msg) {
-    var out = "";
+    var out = [];
     var lastDay = new Date().getDate();
     var first = true;
     var printedToday = false;
     var printedTomorrow = false;
-    var printedLater = false;
+    var template = document.getElementById("lessonTemplate");
     for (var e in msg) {
+        var clone = template.content.cloneNode(true);
         var event = msg[e];
-        var html = "";
+        var html = null;
 
 
         if (!printedToday && event['custom']['today']) {
-            html += "<h3 style='text-align: center'>Heute</h3>";
+            html = document.createElement("h3");
+            html.innerHTML = "Heute";
+            // html = "<h3 style='text-align: center'>Heute</h3>";
             printedToday = true;
         }
         if (!printedTomorrow && event['custom']['tomorrow']) {
-            html += "<h3 style='text-align: center'>Morgen</h3>";
+            html = document.createElement("h3");
+            html.innerHTML = "Morgen";
+            // html = "<h3 style='text-align: center'>Morgen</h3>";
             printedTomorrow = true;
         }
-        if (!event['custom']['tomorrow'] && !event['custom']['today'] && new Date(event['custom']['begin']['date']).getDay() != lastDay) {
-            if (!first) html += "<hr>";
-            html += "<h3 style='text-align: center'>" + new Date(event['custom']['begin']['date']).toLocaleDateString() + "</h3>";
+        if (!event['custom']['tomorrow'] && !event['custom']['today'] && new Date(event['custom']['begin']['date']).getDay() !== lastDay) {
+            html = document.createElement("h3");
+            html.innerHTML = new Date(event['custom']['begin']['date']).toLocaleDateString();
+            // html = "<h3 style='text-align: center'>" + new Date(event['custom']['begin']['date']).toLocaleDateString() + "</h3>";
         }
         if (first) lastDay = new Date(event['custom']['begin']['date']).getDay();
-        if (new Date(event['custom']['begin']['date']).getDay() != lastDay && !first) {
+        if (new Date(event['custom']['begin']['date']).getDay() !== lastDay && !first) {
             lastDay = new Date(event['custom']['begin']['date']).getDay();
+            out.push(document.createElement("hr"));
         }
-        html += "<blockquote class='" + ((event['custom']['isSeminar']) ? "seminar" : "") + ((event['custom']['isKlausur']) ? "klausur" : "") + ((event['custom']['isOnline']) ? " online" : "") + "'><div class=\"row row-top\">" +
-            "<span class=\"column column-20\">" + event['SUMMARY'] + "</span><span\n" +
-            "                    class=\"column-offset-50 column-30 column\" style='text-align: right'>" + ((event['custom']['isOnline']) ? "Online<br><s>" + event['LOCATION'] + "</s>" : event['LOCATION']) + "</span></div>" +
-            "<div class='row'><small class='column'>" + event['DESCRIPTION'] + "</small></div><br>";
 
+        if (html != null) {
+            html.setAttribute("style", "text-align: center;");
+            out.push(html);
+        }
+
+        var blockquote = clone.querySelector("blockquote");
+        if (event['custom']['isKlausur']) {
+            blockquote.classList.add("klausur")
+        }
+        if (event['custom']['isSeminar']) {
+            blockquote.classList.add("seminar")
+        }
+        if (event['custom']['isOnline']) {
+            blockquote.classList.add("online")
+        }
+
+        clone.querySelector("[data-role=\"title\"]").innerText = event['SUMMARY'];
+        clone.querySelector("[data-role=\"description\"]").innerText = event['DESCRIPTION'];
+        clone.querySelector("[data-role=\"location\"]").innerText = ((event['custom']['isOnline']) ? "Online" : event['LOCATION']);
+        var notes = clone.querySelector("[data-role=\"notes\"]");
         if (event['custom']['note']) {
-            html += "<div class='message'>" + event['custom']['note'] + "</div>";
+            notes.innerHTML = event['custom']['note'];
+        } else {
+            notes.innerHTML = "";
+            notes.setAttribute("style", "display: none;");
         }
+        var loggedInNotes = clone.querySelector("[data-role=\"loggedinnotes\"]")
         if (event['custom']['loggedInNote']) {
-            html += "<div class='message'>" + event['custom']['loggedInNote'] + "</div>";
+            loggedInNotes.innerHTML = event['custom']['loggedInNote'];
+        } else {
+            loggedInNotes.innerHTML = "";
+            loggedInNotes.setAttribute("style", "display: none;");
         }
+        var editLink = clone.querySelector("[data-role=\"editnotes\"]");
         if (event['custom']['can_edit']) {
-            html += "<a class='' href='/stundenplan/edit/" + event['custom']['can_edit'] + "'>📝</a>";
+            editLink.setAttribute("href", "/stundenplan/edit/" + event['custom']['can_edit']);
+        } else {
+            editLink.setAttribute("style", "display: none;");
         }
+        var deleteLink = clone.querySelector("[data-role=\"deletenotes\"]");
         if (event['custom']['can_delete']) {
-            if (event['custom']['can_edit']) {
-                html += "<br>";
+            var links = deleteLink.getElementsByTagName("a");
+            if (!event['custom']['note']) {
+                links[0].setAttribute("style", "display: none;");
             }
-            html += event['custom']['can_delete'] + "<br><br>";
+            if (!event['custom']['loggedInNote']) {
+                links[1].setAttribute("style", "display: none;");
+            }
+            if (!event['custom']['loggedInNote'] || !event['custom']['note']) {
+                links[2].setAttribute("style", "display: none;");
+            }
+            for (const link in links) {
+                var link1 = links[link];
+                try {
+                    var attribute = link1.getAttribute("href");
+                    var replace = attribute.replace("__UID__", event['UID']);
+                    link1.setAttribute("href", replace);
+                } catch (e) {
+                }
+            }
+        } else {
+            deleteLink.remove();
         }
 
-        html += "<div class='row mobile-margin-down'>\n" +
-            "                    <div class='column column-20'>Beginn:</div>\n" +
-            "                    <div class='column column-80'>" + event['custom']['begin']['nice'] + "\n" +
-            "                        (" + event['custom']['begin']['words'] + ")\n" +
-            "                    </div></div><div class='row'> \n" +
-            "                    <div class='column column-20'>Ende:</div>\n" +
-            "                    <div class='column column-80'>" + event['custom']['end']['nice'] + ((event['custom']['begin']['isPast']) ? " (" + event['custom']['end']['words'] + ")" : "") + "</div></div>\n";
+        clone.querySelector("[data-role=\"beginntime\"]").innerText = event['custom']['begin']['nice'] + " (" + event['custom']['begin']['words'] + ")";
+        clone.querySelector("[data-role=\"endtime\"]").innerText = event['custom']['end']['nice'] + " (" + event['custom']['end']['words'] + ")";
+
         if (event['custom']['current']) {
             var percent = Number(((Date.now() / 1000) - event['custom']['begin']['timestamp']) / (event['custom']['end']['timestamp'] - event['custom']['begin']['timestamp'])) * 100;
-            html += "                        <br><div class='column'>\n" +
-                "                            <div class=\"progress\">\n" +
-                "                                <div class=\"progress-value\" data-percent='" + Math.round(percent) + "' style=\"width: " + percent + "%\">" + percent.toFixed(0) + "%</div>\n" +
-                "                            </div>\n" +
-                "                        </div>\n";
+            var progressbar = clone.querySelector("[data-role=\"progressbar\"]");
+            progressbar.setAttribute("data-percent", Math.round(percent) + "")
+            progressbar.setAttribute("style", "width: " + percent + "%")
+            progressbar.innerHTML = percent.toFixed(0) + "%";
+        } else {
+            clone.querySelector("[data-role=\"progress-wrapper\"]").remove()
         }
-
-        out += html + "</blockquote>";
+        // "<div class=\"progress-value\" data-percent='" + Math.round(percent) + "' style=\"width: " + percent + "%\">" + percent.toFixed(0) + "%</div>\n";
         first = false;
+        out.push(clone);
     }
-    $('#list').html(out);
+    $('#list').html("");
+    for (const outKey of out) {
+        $('#list').append(outKey);
+    }
 }
 
 $('#courseSelector').on('change', function () {
