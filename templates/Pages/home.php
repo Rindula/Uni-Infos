275cefb200c40006a921c6911c91c7283054d5a4
Sven Nolting <Rindula@users.noreply.github.com>
2021-04-24 11:32:43 +0200

    refactor: anpassung Startseite

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -5,10 +5,14 @@
 
 use App\View\AppView; ?>
 <h1 style="width: 100%; text-align: center">UniInfos</h1>
-<h2 style="width: 100%; text-align: center">Links</h2>
-<div style="text-align: center">
-    <?= $this->Html->link(__("To file storage"), 'https://files.rindula.de/nextcloud/index.php/s/FffjYnKGNjYiNMb', ['class' => 'button button-outline', 'target' => '_blank']) ?>
-    <?= $this->Html->link(__("To the timetable"), ['controller' => 'stundenplan', 'action' => 'index'], ['class' => 'button button-clear']) ?>
-    <!--    <br>-->
-    <?= ""//$this->Html->link("<img style='width: 200px' alt='Jetzt bei Google Play' src='https://play.google.com/intl/en_us/badges/static/images/badges/de_badge_web_generic.png'/>", 'https://play.google.com/store/apps/details?id=de.rindula.uniinfos&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1', ['target' => '_blank', 'escape' => false])  ?>
+<h2 style="width: 100%; text-align: center"><?= __("The unofficial webbased timetable for the DHBW Mosbach") ?></h2>
+<hr>
+<h3><?=__("What can you do with it?")?></h3>
+<div>
+    <p>&gt; <?= __("View the Timetable for your class") ?></p>
+    <p>&gt; <?= __("Get a free Rest API to interface with any timetable") ?></p>
+    <p>&gt; <?= __("Get a link to import the timetable into your calendar, even filtered by onlineclasses only") ?></p>
 </div>
+<hr>
+<h3><?=__("Have a featurerequest or improvements?")?></h3>
+<a href="#" style="color: #1957f5;"><?= __("View this project on GitHub") ?></a>
